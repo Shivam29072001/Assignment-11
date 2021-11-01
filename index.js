35457218c7dc6c2bedd21986f609c10ad48f4dfe
Shivam29072001 <90239996+Shivam29072001@users.noreply.github.com>
2021-11-01 08:48:11 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+function display() {
+    var x; //undefine
+
+    console.log(x);
+
+
+    console.log(z);
+
+    var z = 60; //undefine
+
+    console.log(w); // not define
+    function Alert() {
+        alert("value of x is: " + "undefined\n" + "value of w is: " + "Not defined");
+    }
+    Alert();
+}
